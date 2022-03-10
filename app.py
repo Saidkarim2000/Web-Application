cb3d9f5c1aa0343b908371450944638b87edc426
Saidkarim2000 <yuldashev754@gmail.com>
2022-03-10 16:35:42 +0500

    comment predict_label function

--- app.py
+++ app.py
@@ -9,20 +9,20 @@ from flask import Flask, render_template, request, send_from_directory
 app = Flask(__name__)
 
 
-model_pkl = pickle.load(open('cats_vs_dogs_model.pkl','rb'))
+# model_pkl = pickle.load(open('cats_vs_dogs_model.pkl','rb'))
 
-def predict_label(f_path):
-    img = image.load_img(f_path, target_size=(150, 150))
-    x = image.img_to_array(img)
-    x /= 255
-    x = np.expand_dims(x, axis=0)
-    images = np.vstack([x])
-    classes = model_pkl.predict(images, batch_size=10)
-    print(classes[0])
-    if classes[0]>0.5:
-        return (" Dog")
-    else:
-        return (" Cat")            
+# def predict_label(f_path):
+#     img = image.load_img(f_path, target_size=(150, 150))
+#     x = image.img_to_array(img)
+#     x /= 255
+#     x = np.expand_dims(x, axis=0)
+#     images = np.vstack([x])
+#     classes = model_pkl.predict(images, batch_size=10)
+#     print(classes[0])
+#     if classes[0]>0.5:
+#         return (" Dog")
+#     else:
+#         return (" Cat")            
 
 path = app.config["UPLOADED_IMAGES"] = "static"
 
